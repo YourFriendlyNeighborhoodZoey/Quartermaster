b2a01dd0ddf698c68b8cfa44aa5ee619966b9673
Zoey <YourFriendlyNeighborhoodZoey@gmail.com>
2025-06-18 20:45:02 -0700

    Create ItemEntry & ItemList Classes

---
+++ ledger/lists/list.py
@@ -0,0 +1,30 @@
+"""Script Docstring
+"""
+#import [package/module]
+import logging
+
+module_logger = logging.Logger()
+
+
+class ItemEntry:
+    """
+    """
+    def __init__(self):
+        """
+        """
+class ItemList:
+    """
+    """
+    def __init__(self):
+        """
+        """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+else:
+    module_logger.info("%s has been succesfully imported!" % __name__)
