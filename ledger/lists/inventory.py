01cc1e8278df0f3f824083fd42c521b7891defdb
Zoey <YourFriendlyNeighborhoodZoey@gmail.com>
2025-06-18 20:52:09 -0700

    Create Inventory & Shopping.py

---
+++ ledger/lists/inventory.py
@@ -0,0 +1,33 @@
+"""Script Docstring
+"""
+#import [package/module]
+from list import ItemEntry, ItemList
+
+import logging
+
+
+module_logger = logging.Logger()
+
+
+class InventoryEntry(ItemEntry):
+    """
+    """
+    def __init__(self):
+        """
+        """
+class InventoryList(ItemList):
+    """
+    """
+    def __init__(self):
+        """
+        """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+else:
+    module_logger.info("%s has been succesfully imported!" % __name__)
---
+++ ledger/lists/shopping.py
@@ -0,0 +1,33 @@
+"""Script Docstring
+"""
+#import [package/module]
+from list import ItemEntry, ItemList
+
+import logging
+
+
+module_logger = logging.Logger()
+
+
+class ShoppingEntry(ItemEntry):
+    """
+    """
+    def __init__(self):
+        """
+        """
+class ShoppingList(ItemList):
+    """
+    """
+    def __init__(self):
+        """
+        """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+else:
+    module_logger.info("%s has been succesfully imported!" % __name__)
